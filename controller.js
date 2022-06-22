606190119dfbedb5bb381e5824e501232db04a16
PagiY <pagiyap003@gmail.com>
2022-06-22 23:52:27 +0800

    cleaned codes

--- controller.js
+++ controller.js
@@ -1,22 +1,11 @@
 import {gameobjects} from './gameobjects.js';
 
 let keys = {
-    'left'  : {pressed: false},
-    'up'    : {pressed: false},
-    'right' : {pressed: false},
-    'down'  : {pressed: false},
-    'currentkey': '',
     'checkkey': '',
-    'nextkey': '',
 }
 
 class Controller {
     constructor() {
-        
-    }
-    
-    updateKey(){
-        keys.currentkey = keys.nextkey;
         
     }
 
@@ -24,21 +13,16 @@ class Controller {
 
         switch(e.keyCode){
             case 38:
-                
                 keys.checkkey = 'up'
-                keys.up.pressed = true;
                 break;
             case 37:
                 keys.checkkey = 'left'
-                keys.left.pressed = true;
                 break;
             case 39:
                 keys.checkkey = 'right'
-                keys.right.pressed = true;
                 break;
             case 40:
                 keys.checkkey = 'down'
-                keys.down.pressed = true;
                 break;
          }
 
@@ -46,22 +30,6 @@ class Controller {
       
     }
 
-    keyPressFalse(e){
-        switch(e.keyCode){
-            case 38:
-                keys.up.pressed = false;
-                break;
-            case 37:
-                keys.left.pressed = false;
-                break;
-            case 39:
-                keys.right.pressed = false;
-                break;
-            case 40:
-                keys.down.pressed = false;
-                break;
-         } 
-    }
 }
 
 export {Controller, keys};
