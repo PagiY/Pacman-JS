909be1d892f6956fb4783f91d0a1a93faaa3a91e
PagiY <pagiyap003@gmail.com>
2022-06-22 23:45:45 +0800

    updates

--- display.js
+++ display.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import {Brick, Pacman, gameobjects, defaults} from './gameobjects.js'
+import {Brick, Pacman, gameobjects, defaults, Space} from './gameobjects.js'
 
 let canvas;
 let ctx;
@@ -19,14 +19,25 @@ function initialize(){
 
 }
 
+//30, 70, 110, 150, 190, ...
+//this.x - [30, 70, 110, 140, or 170]
+//compare and get smallest difference
+//the result with smallest difference is the new position 
+//
 let world = [
     ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'],
     ['1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'],
+    ['1', '0', '1', '0', '1', '0', '1', '1', '0', '1', '1', '0', '1'],
+    ['1', '0', '0', '0', '1', '0', '1', '1', '0', '1', '1', '0', '1'],
+    ['1', '0', '1', '0', '1', '0', '1', '1', '0', '1', '1', '0', '1'],
+    ['1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '1'],
+    ['1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '1'],
     ['1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'],
-    ['1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'],
+    ['1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '1'],
     ['1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1'],
     ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'],
 ]
+
 
 function createGameWorld(){
 
@@ -35,6 +46,9 @@ function createGameWorld(){
             if(obj === '1'){
                 gameobjects.bricksList.push(new Brick(ctx, j * defaults.w, i * defaults.h))
             }
+            else if(obj === '0'){
+                gameobjects.spaceList.push(new Space(ctx, j * defaults.w, i * defaults.h))
+            }
             
         })
     })
@@ -42,24 +56,39 @@ function createGameWorld(){
     gameobjects.bricksList.forEach((brick) => {
         brick.draw()
     })
+
+    gameobjects.spaceList.forEach((space) => {
+        space.draw()
+    })
+
+    
 }
 
 function createPacman(){
     
-    gameobjects.pacman = new Pacman(ctx, defaults.w / 2, defaults.h / 2)
-    gameobjects.pacman.draw()
+    gameobjects.pacman = new Pacman(ctx, 20 + defaults.w / 2, 20 + defaults.h / 2)
+    gameobjects.pacman.draw();
+    gameobjects.pacman.getCurSpace();
+    gameobjects.pacman.checkDirections();
 
 }
 
 function gameLoop(){
 
-    ctx.clearRect(0,0,canvas.width, canvas.height);
-    gameobjects.pacman.update();
     
-    gameobjects.bricksList.forEach((brick) => {
-        brick.draw()
-    })
+    ctx.clearRect(0,0,canvas.width, canvas.height);
     gameobjects.pacman.draw();
+    gameobjects.pacman.update();
+
+    gameobjects.bricksList.forEach((brick) => {
+        //gameobjects.pacman.detectCollision(brick)
+        brick.draw()
+
+    })
+
+    gameobjects.spaceList.forEach((space) => {
+        space.draw()
+    })
 
     window.requestAnimationFrame(gameLoop);
 
